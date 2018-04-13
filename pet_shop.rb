7b98468ea866d507500c162f571c0666eebabfe6
Duncan <marjoribanks.d@gmail.com>
2018-04-13 14:36:37 +0100

    pets by breed test passed

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,23 +1,36 @@
-def pet_shop_name(shop_name)
-  return shop_name[:name]
-end
+# def pet_shop_name(shop_name)
+#   return shop_name[:name]
+# end
+#
+# def total_cash(shop)
+#   return shop[:admin][:total_cash]
+# end
 
-def total_cash(shop)
-  return shop[:admin][:total_cash]
-end
 
-def add_or_remove_cash(shop, amount)
-  return shop[:admin][:total_cash] += amount
-end
+# def add_or_remove_cash(shop, amount)
+#   return shop[:admin][:total_cash] += amount
+# end
+#
+# def total_cash(shop)
+#   return shop[:admin][:total_cash]
+# end
 
-def pets_sold(shop)
- return shop[:admin][:pets_sold]
-end
+# def pets_sold(shop)
+#  return shop[:admin][:pets_sold]
+# end
+#
+# def increase_pets_sold(shop, sold)
+#   return shop[:admin][:pets_sold] =+ sold
+# end
 
-def increase_pets_sold(shop, sold)
-  return shop[:admin][:pets_sold] =+ sold
+# def stock_count(shop)
+#   return shop[:pets].length
+# end
+#
+def pets_by_breed(shop, breed)
+total_breed_count = []
+for breed in shop[:pets][:breed]
+  total_breed_count += 1
 end
-
-def stock_count(shop)
-  return shop[:pets].length
+return total_breed_count
 end
